ed189a5cf4981eb78ed406531c9195ae126884c7
Isaac <isaaclanzoni99@gmail.com>
2025-01-31 19:18:05 -0300

    atualizando

--- home/home.js
+++ home/home.js
@@ -27,9 +27,58 @@ uploadButton.addEventListener('click', () => {
 
     setTimeout(() => {
         processFile();
+        addSaveInformations();
+        const visual = window.document.querySelector('.poke-container');
+        visual.style.display = 'block';
     }, 10000);
     
 });
+
+
+function addSaveInformations() {
+    const divPokeContainer = document.createElement('div');
+    const divTexts = document.createElement('div');
+    const h2 = document.createElement('h2');
+    const h4I = document.createElement('h4');
+    const h4II = document.createElement('h4');
+    const divOwnedInfo = document.createElement('div');
+    const h6I = document.createElement('h6');
+    const h6II = document.createElement('h6');
+    const divSeenInfo = document.createElement('div');
+    const main = document.getElementById('main');
+    const img = document.createElement('img');
+    const imgII = document.createElement('img');
+
+
+    h2.innerText = 'Pokémons';
+    h4I.innerText = 'Capturados';
+    h4II.innerText = 'Vistos';
+    h6I.innerText = 'Charmander';
+    h6II.innerText = 'Charmander';
+    img.src = "https://img.pokemondb.net/sprites/sword-shield/normal/charmander.png";
+    img.alt = "charmander-icon";
+    imgII.src = "https://img.pokemondb.net/sprites/sword-shield/normal/charmander.png";
+    imgII.alt = "charmander-icon";
+
+    divPokeContainer.classList.add('poke-container');
+    divTexts.classList.add('texts');
+    h4I.classList.add('owned');
+    h4II.classList.add('seen');
+    divOwnedInfo.classList.add('owned-info');
+    divSeenInfo.classList.add('seen-info');
+
+    main.appendChild(divPokeContainer);
+    divPokeContainer.appendChild(divTexts);
+    divTexts.appendChild(h2);
+    divTexts.appendChild(h4I);
+    divTexts.appendChild(h4II);
+    h4I.appendChild(divOwnedInfo);
+    h4II.appendChild(divSeenInfo);
+    divOwnedInfo.appendChild(h6I);
+    divSeenInfo.appendChild(h6II);
+    h6I.appendChild(imgII);
+    h6II.appendChild(img);    
+}
 
 
 const form = {
